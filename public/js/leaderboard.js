6d0b3d5876b5415ffdfc32643adb0c671195e937
Emilio Zand <emilio.zand@sigfig.com>
2017-06-14 14:05:26 -0700

    Add partials for leaderboard and recent matches

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -2,16 +2,14 @@ $(document).ready(function() {
 
   $.ajax({
     type: "GET",
-    url: "api/season?team_id=59408e751865720006b81b9d",
+    url: "api/seasons/current?team_id=59408e751865720006b81b9d",
     success: function(data) {
-      for (var user in data.users_ranks) {
+      var ranked_users = data.users_rank;
+      for (var user in ranked_users) {
         if(user.winning_streak != 0) {
           user.streak = 'W' + user.winning_streak;
         } else {
           user.streak = 'L' + user.losing_streak
-        }
-        if(user.rank) {
-          ranked_users.append(user);
         }
       }
 
@@ -22,7 +20,7 @@ $(document).ready(function() {
       for(var user in ranked_users){
          $table.append( '<tr><td>' + user.rank + '</td><td>' + user.user_name + '</td><td>'+ (user.elo + 1200) + '</td><td>' + user.wins + '</td><td>' + user.loses + '</td><td>' + user.streak + '</td></tr>' );
       }
-
+      console.log('Did we do it?');
       $('#leaderboard').append(table);
     },
   });
--- public/js/matches.js
+++ public/js/matches.js
@@ -4,19 +4,20 @@ $(document).ready(function() {
     type: "GET",
     url: "api/matches?team_id=59408e751865720006b81b9d",
     success: function(data) {
-      var active_teams_count = 0;
-      var users_count = 0;
-      var matches_count = 0;
+      var table = $('<table></table>').addClass('recent-matches');
+      $table.append( '<th><td>' + 'Winner' + '</td><td>' + 'Loser' + '</td><td>'+ 'Score' + '</td><td>' + "ELO Change" + '</td></th>' );
       for (var match in data.matches) {
-        game = data.games[game];
-        active_teams_count += game.active_teams_count;
-        users_count += game.users_count;
-        matches_count += game.matches_count;
+        var games = match.scores;
+        var loserScore = 0;
+        var winnerScore = 0;
+        var eloChange = match.winners[0].elo_history.pop();
+        for(score in games){
+          score[0] > score[1] ? loserScore++ : winnerScore++;
+        }
+        var overallScore = winnerScore + ' : ' + loserScore
+        $table.append( '<tr><td>' + match.winners[0].user_name + '</td><td>' + match.score[0].user_name + '</td><td>'+ overallScore + '</td><td>' + eloChange + '</td></tr>' );
       }
-      $('#matches').hide().text(
-        active_teams_count + " active teams with " + matches_count + ' games played by ' + users_count + " players!"
-      ).fadeIn('slow');
+      $('#recent-matches').append(table);
     },
   });
-
 });
