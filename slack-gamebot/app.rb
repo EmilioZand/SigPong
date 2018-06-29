63f6647e3b70c7476b3cc2f63479a2e97b4920e3
Emilio Zand <emilio.zand@sigfig.com>
2018-06-29 10:34:39 -0700

    Fix config.ru to ensure that the after_start! thread starts on app init

--- slack-gamebot/app.rb
+++ slack-gamebot/app.rb
@@ -1,11 +1,8 @@
+require 'celluloid'
+
 module SlackGamebot
   class App < SlackRubyBotServer::App
     include Celluloid
-
-    DEAD_MESSAGE = <<-EOS.freeze
-This leaderboard has been dead for over a month, deactivating.
-Re-install the bot at https://www.playplay.io. Your data will be purged in 2 weeks.
-EOS
 
     def after_start!
       once_and_every 60 * 3 do
